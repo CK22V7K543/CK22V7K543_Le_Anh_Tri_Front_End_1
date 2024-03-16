c91adca6abd5927c92fdfddddf0c76f1060ecefb
CK22V7K543 <trick22v7k543@vlvh.ctu.edu.vn>
2024-03-16 14:47:35 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router"; // Import router từ file router.js
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+//import "./assets/main.css";
+// Tạo ứng dụng và sử dụng router, sau đó mount ứng dụng vào #app
+createApp(App).use(router).mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-port: 3001,
-},
+    port: 3001,
+    proxy: {
+      "/api": {
+      target: "http://localhost:3000/",
+      changeOrigin: true,
+      },
+    }
+  },
   plugins: [
     vue(),
   ],
@@ -16,4 +22,4 @@ port: 3001,
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
