b798eaae106eb71eacb617730cdcdc25e9747d39
CK22V7K543 <trick22v7k543@vlvh.ctu.edu.vn>
2024-03-17 09:11:30 +0700

    Tao trang them moi lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-if="!contact" class="page">
+        <h4>Thêm Liên hệ Mới</h4>
+        <ContactForm @submit:contact="addContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: null,
+            message: "",
+        };
+    },
+    methods: {
+        async addContact(data) {
+            try {
+                await ContactService.create(data);
+                alert('Liên hệ đã được thêm thành công.');
+                this.$router.push({ name: "contactbook" });
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+};
+</script>
